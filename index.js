9704e6c47cf5546708f22fa3fd5a5fc080b0f377
RKANEBeats <armeanumihai2008@gmail.com>
2024-04-06 14:05:39 +0300

    Add condition that outputs computer's choice

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
 /* Create initial function that selects random choice from "Rock, Paper, Scissors" */
-
 function getComputerChoice () {
     let choices = ["Rock" , "Paper", "Scissors"]
-    let randomizeChoice = Math.floor(Math.random() * choices.length)
-    console.log(randomizeChoice)
+    let randomizeChoice = Math.floor(Math.random() * choices.length) + 1
+    
+    if (randomizeChoice === 1) {
+        return  "Rock"
+    }  if (randomizeChoice === 2) {
+        return  "Paper"
+    } if (randomizeChoice === 3) {
+        return  "Scissors"
+    }
 }
-
-getComputerChoice();
+        
+console.log (getComputerChoice());
