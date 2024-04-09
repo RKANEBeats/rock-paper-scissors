a3036ae810de3dba764b73843b2e81d3ed3dc560
RKANEBeats <armeanumihai2008@gmail.com>
2024-04-09 17:52:44 +0100

    Add function that runs a single round

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 
 /* Create initial function that selects random choice from "Rock, Paper, Scissors" */
 function getComputerChoice () {
-    let choices = ["Rock" , "Paper", "Scissors"]
+    let choices = ["rock" , "paper", "scissors"]
     let randomizeChoice = Math.floor(Math.random() * choices.length) + 1
     
     if (randomizeChoice === 1) {
-        return  "Rock"
+        return  "rock"
     }  if (randomizeChoice === 2) {
-        return  "Paper"
+        return  "paper"
     } if (randomizeChoice === 3) {
-        return  "Scissors"
+        return  "scissors"
     }
 }
-        
-console.log (getComputerChoice());
+    
+
+/* Create new function that plays a single round of RPS */
+
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === "rock" && computerSelection === "paper") 
+    return "You lose! Paper beats Rock."
+    else if (playerSelection === "rock" && computerSelection === "scissors")
+    return "You win! Rock beats Scissors."
+    else if (playerSelection === computerSelection)
+    return "It's a Draw! Both chose Rock."
+
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
